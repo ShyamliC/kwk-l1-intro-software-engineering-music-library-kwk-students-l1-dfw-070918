72f80e44484c22fa5e2dad2e5f02340ca38b9d35
ShyamliC <shyamli.channa@gmail.com>
2018-07-11 21:38:17 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -19,7 +19,7 @@ end
 #4. This method should add a new artist and song (key value pair) to the hash and return the updated hash. 
 # This method should accept three arguments, the library hash, the artist, and the song.
 def add_song_and_artist(add_hash,artist,song)
-  add_hash_artist[]=song
+  add_hash[artist]=song
   add_hash
 end
 
